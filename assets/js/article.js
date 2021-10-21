f465bd701d12a1ad8b8ebf273094ddbe81e7ba1a
David Van Goidtsnoven <vangoidtsnovendavid@gmail.com>
2021-10-21 11:34:09 +0200

    push

--- assets/js/article.js
+++ assets/js/article.js
@@ -52,45 +52,98 @@ clickAlerte("demoAlerte", "Votre texte ici");
 clickAlerte("demoAlerte2", "Votre texte ici");
 
 // listen for event
-document.querySelectorAll('.exempleAnimate').forEach((element) =>
-    element.addEventListener('click', () => {
-        if (document.querySelector("#" + element.id).dataset.status==="0") checkLetterChosen(element.id);
-    }));
 
-id("my-element1").addEventListener("click", ()=> {
-    animateCss('#my-element1', 'bounce');
-})
-id("my-element2").addEventListener("click", ()=> {
-    animateCss('#my-element2', 'flash');
-})
-id("my-element3").addEventListener("click", ()=> {
-    animateCss('#my-element3', 'pulse');
-})
-id("my-element4").addEventListener("click", ()=> {
-    animateCss('#my-element4', 'rubberBand');
-})
-/*    <div id="my-element2" className="exempleAnimate">animateCss('#my-element2', 'flash');</div>
-    <div id="my-element3" className="exempleAnimate">animateCss('#my-element3', 'pulse');</div>
-    <div id="my-element4" className="exempleAnimate">animateCss('#my-element4', 'rubberBand');</div>
-    <div id="my-element5" className="exempleAnimate">animateCss('#my-element5', 'shakeX');</div>
-    <div id="my-element6" className="exempleAnimate">animateCss('#my-element6', 'shakeY');</div>
-    <div id="my-element7" className="exempleAnimate">animateCss('#my-element7', 'headShake');</div>
-    <div id="my-element8" className="exempleAnimate">animateCss('#my-element8', 'swing');</div>
-    <div id="my-element9" className="exempleAnimate">animateCss('#my-element9', 'tada');</div>
-    <div id="my-element10" className="exempleAnimate">animateCss('#my-element10', 'wobble');</div>
-    <div id="my-element11" className="exempleAnimate">animateCss('#my-element11', 'jello');</div>
-    <div id="my-element12" className="exempleAnimate">animateCss('#my-element12', 'heartBeat');</div>
-    <div id="my-element13" className="exempleAnimate">animateCss('#my-element13', 'backInDown');</div>
-    <div id="my-element14" className="exempleAnimate">animateCss('#my-element14', 'backInLeft');</div>
-    <div id="my-element15" className="exempleAnimate">animateCss('#my-element15', 'backInRight');</div>
-    <div id="my-element16" className="exempleAnimate">animateCss('#my-element16', 'backInUp');</div>
-    <div id="my-element17" className="exempleAnimate">animateCss('#my-element17', 'bounceIn');</div>
-    <div id="my-element18" className="exempleAnimate">animateCss('#my-element18', 'bounceInDown');</div>
-    <div id="my-element19" className="exempleAnimate">animateCss('#my-element19', 'bounceInLeft');</div>
-    <div id="my-element20" className="exempleAnimate">animateCss('#my-element20', 'bounceInRight');</div>
-    <div id="my-element21" className="exempleAnimate">animateCss('#my-element21', 'bounceInUp');</div>
-    <div id="my-element22" className="exempleAnimate">animateCss('#my-element22', 'bounceOut');</div>
-    <div id="my-element23" className="exempleAnimate">animateCss('#my-element23', 'bounceOutDown');</div>
-    <div id="my-element24" className="exempleAnimate">animateCss('#my-element24', 'bounceOutLeft');</div>
-    <div id="my-element25" className="exempleAnimate">animateCss('#my-element25', 'bounceOutRight');</div>
-    <div id="my-element26" className="exempleAnimate">animateCss('#my-element26', 'bounceOutUp');</div>*/
+id("my-element1").addEventListener("click", ()=> { animateCss('#my-element1', 'bounce'); })
+id("my-element2").addEventListener("click", ()=> { animateCss('#my-element2', 'flash'); })
+id("my-element3").addEventListener("click", ()=> { animateCss('#my-element3', 'pulse'); })
+id("my-element4").addEventListener("click", ()=> { animateCss('#my-element4', 'rubberBand'); })
+id("my-element5").addEventListener("click", ()=> { animateCss('#my-element5', 'shakeX'); })
+id("my-element6").addEventListener("click", ()=> { animateCss('#my-element6', 'shakeY'); })
+id("my-element7").addEventListener("click", ()=> { animateCss('#my-element7', 'headShake'); })
+id("my-element8").addEventListener("click", ()=> { animateCss('#my-element8', 'swing'); })
+id("my-element9").addEventListener("click", ()=> { animateCss('#my-element9', 'tada'); })
+id("my-element10").addEventListener("click", ()=> { animateCss('#my-element10', 'wobble'); })
+id("my-element11").addEventListener("click", ()=> { animateCss('#my-element11', 'jello'); })
+id("my-element12").addEventListener("click", ()=> { animateCss('#my-element12', 'heartBeat'); })
+id("my-element13").addEventListener("click", ()=> { animateCss('#my-element13', 'backInDown'); })
+id("my-element14").addEventListener("click", ()=> { animateCss('#my-element14', 'backInLeft'); })
+id("my-element15").addEventListener("click", ()=> { animateCss('#my-element15', 'backInRight'); })
+id("my-element16").addEventListener("click", ()=> { animateCss('#my-element16', 'backInUp'); })
+id("my-element17").addEventListener("click", ()=> { animateCss('#my-element17', 'bounceIn'); })
+id("my-element18").addEventListener("click", ()=> { animateCss('#my-element18', 'bounceInDown'); })
+id("my-element19").addEventListener("click", ()=> { animateCss('#my-element19', 'bounceInLeft'); })
+id("my-element20").addEventListener("click", ()=> { animateCss('#my-element20', 'bounceInRight'); })
+id("my-element21").addEventListener("click", ()=> { animateCss('#my-element21', 'bounceInUp'); })
+id("my-element22").addEventListener("click", ()=> { animateCss('#my-element22', 'bounceOut'); })
+id("my-element23").addEventListener("click", ()=> { animateCss('#my-element23', 'bounceOutDown'); })
+id("my-element24").addEventListener("click", ()=> { animateCss('#my-element24', 'bounceOutLeft'); })
+id("my-element25").addEventListener("click", ()=> { animateCss('#my-element25', 'bounceOutRight'); })
+id("my-element26").addEventListener("click", ()=> { animateCss('#my-element26', 'bounceOutUp'); })
+id("my-element27").addEventListener("click", ()=> { animateCss('#my-element27', 'fadeInDown'); })
+id("my-element28").addEventListener("click", ()=> { animateCss('#my-element28', 'fadeInDown'); })
+id("my-element29").addEventListener("click", ()=> { animateCss('#my-element29', 'fadeInDownBig'); })
+id("my-element30").addEventListener("click", ()=> { animateCss('#my-element30', 'fadeInLeft'); })
+id("my-element31").addEventListener("click", ()=> { animateCss('#my-element31', 'fadeInLeftBig'); })
+id("my-element32").addEventListener("click", ()=> { animateCss('#my-element32', 'fadeInRight'); })
+id("my-element33").addEventListener("click", ()=> { animateCss('#my-element33', 'fadeInRightBig'); })
+id("my-element34").addEventListener("click", ()=> { animateCss('#my-element34', 'fadeInUp'); })
+id("my-element35").addEventListener("click", ()=> { animateCss('#my-element35', 'fadeInUpBig'); })
+id("my-element36").addEventListener("click", ()=> { animateCss('#my-element36', 'fadeInTopLeft'); })
+id("my-element37").addEventListener("click", ()=> { animateCss('#my-element37', 'fadeInTopRight'); })
+id("my-element38").addEventListener("click", ()=> { animateCss('#my-element38', 'fadeInBottomLeft'); })
+id("my-element39").addEventListener("click", ()=> { animateCss('#my-element39', 'fadeInBottomRight'); })
+id("my-element40").addEventListener("click", ()=> { animateCss('#my-element40', 'fadeOut'); })
+id("my-element41").addEventListener("click", ()=> { animateCss('#my-element41', 'fadeOutDown'); })
+id("my-element42").addEventListener("click", ()=> { animateCss('#my-element42', 'fadeOutDownBig'); })
+id("my-element43").addEventListener("click", ()=> { animateCss('#my-element43', 'fadeOutLeft'); })
+id("my-element44").addEventListener("click", ()=> { animateCss('#my-element44', 'fadeOutLeftBig'); })
+id("my-element45").addEventListener("click", ()=> { animateCss('#my-element45', 'fadeOutRight'); })
+id("my-element46").addEventListener("click", ()=> { animateCss('#my-element46', 'fadeOutRightBig'); })
+id("my-element47").addEventListener("click", ()=> { animateCss('#my-element47', 'fadeOutUp'); })
+id("my-element48").addEventListener("click", ()=> { animateCss('#my-element48', 'fadeOutUpBig'); })
+id("my-element49").addEventListener("click", ()=> { animateCss('#my-element49', 'fadeOutTopLeft'); })
+id("my-element50").addEventListener("click", ()=> { animateCss('#my-element50', 'fadeOutTopRight'); })
+id("my-element51").addEventListener("click", ()=> { animateCss('#my-element51', 'fadeOutBottomRight'); })
+id("my-element52").addEventListener("click", ()=> { animateCss('#my-element52', 'fadeOutBottomLeft'); })
+id("my-element53").addEventListener("click", ()=> { animateCss('#my-element53', 'flip'); })
+id("my-element54").addEventListener("click", ()=> { animateCss('#my-element54', 'flipInX'); })
+id("my-element55").addEventListener("click", ()=> { animateCss('#my-element55', 'flipInY'); })
+id("my-element56").addEventListener("click", ()=> { animateCss('#my-element56', 'flipOutX'); })
+id("my-element57").addEventListener("click", ()=> { animateCss('#my-element57', 'flipOutY'); })
+id("my-element58").addEventListener("click", ()=> { animateCss('#my-element58', 'lightSpeedInRight'); })
+id("my-element59").addEventListener("click", ()=> { animateCss('#my-element59', 'lightSpeedInLeft'); })
+id("my-element60").addEventListener("click", ()=> { animateCss('#my-element60', 'lightSpeedOutRight'); })
+id("my-element61").addEventListener("click", ()=> { animateCss('#my-element61', 'lightSpeedOutLeft'); })
+id("my-element62").addEventListener("click", ()=> { animateCss('#my-element62', 'rotateIn'); })
+id("my-element63").addEventListener("click", ()=> { animateCss('#my-element63', 'rotateInDownLeft'); })
+id("my-element64").addEventListener("click", ()=> { animateCss('#my-element64', 'rotateInDownRight'); })
+id("my-element65").addEventListener("click", ()=> { animateCss('#my-element65', 'rotateInUpLeft'); })
+id("my-element66").addEventListener("click", ()=> { animateCss('#my-element66', 'rotateInUpRight'); })
+id("my-element67").addEventListener("click", ()=> { animateCss('#my-element67', 'rotateOut'); })
+id("my-element68").addEventListener("click", ()=> { animateCss('#my-element68', 'rotateOutDownLeft'); })
+id("my-element69").addEventListener("click", ()=> { animateCss('#my-element69', 'rotateOutDownRight'); })
+id("my-element70").addEventListener("click", ()=> { animateCss('#my-element70', 'rotateOutUpLeft'); })
+id("my-element71").addEventListener("click", ()=> { animateCss('#my-element71', 'rotateOutUpRight'); })
+id("my-element72").addEventListener("click", ()=> { animateCss('#my-element72', 'hinge'); })
+id("my-element73").addEventListener("click", ()=> { animateCss('#my-element73', 'jackInTheBox'); })
+id("my-element74").addEventListener("click", ()=> { animateCss('#my-element74', 'rollIn'); })
+id("my-element75").addEventListener("click", ()=> { animateCss('#my-element75', 'rollOut'); })
+id("my-element76").addEventListener("click", ()=> { animateCss('#my-element76', 'zoomIn'); })
+id("my-element77").addEventListener("click", ()=> { animateCss('#my-element77', 'zoomInDown'); })
+id("my-element78").addEventListener("click", ()=> { animateCss('#my-element78', 'zoomInLeft'); })
+id("my-element79").addEventListener("click", ()=> { animateCss('#my-element79', 'zoomInRight'); })
+id("my-element80").addEventListener("click", ()=> { animateCss('#my-element80', 'zoomInUp'); })
+id("my-element81").addEventListener("click", ()=> { animateCss('#my-element81', 'zoomOut'); })
+id("my-element82").addEventListener("click", ()=> { animateCss('#my-element82', 'zoomOutDown'); })
+id("my-element83").addEventListener("click", ()=> { animateCss('#my-element83', 'zoomOutLeft'); })
+id("my-element84").addEventListener("click", ()=> { animateCss('#my-element84', 'zoomOutRight'); })
+id("my-element85").addEventListener("click", ()=> { animateCss('#my-element85', 'zoomOutUp'); })
+id("my-element86").addEventListener("click", ()=> { animateCss('#my-element86', 'slideInDown'); })
+id("my-element87").addEventListener("click", ()=> { animateCss('#my-element87', 'slideInLeft'); })
+id("my-element88").addEventListener("click", ()=> { animateCss('#my-element88', 'slideInRight'); })
+id("my-element89").addEventListener("click", ()=> { animateCss('#my-element89', 'slideInUp'); })
+id("my-element90").addEventListener("click", ()=> { animateCss('#my-element90', 'slideOutDown'); })
+id("my-element91").addEventListener("click", ()=> { animateCss('#my-element91', 'slideOutLeft'); })
+id("my-element92").addEventListener("click", ()=> { animateCss('#my-element92', 'slideOutRight'); })
+id("my-element93").addEventListener("click", ()=> { animateCss('#my-element93', 'slideOutUp'); })
+id("my-element94").addEventListener("click", ()=> { animateCss('#my-element94', 'slideOutUp').then((message) => { id('my-element94').style.display='none' }) })
