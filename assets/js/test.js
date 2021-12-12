70e08adcb739b9d67ae3855ea5524393a78336f4
David Van Goidtsnoven <vangoidtsnovendavid@gmail.com>
2021-12-12 10:35:30 +0100

    push test

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,16 +1,12 @@
-createMenu("FF333D", "FFFFFF", "4", "90", "");
+createMenu("FF333D", "FFFFFF", 4, "90", "");
 elementMenuLogo ("./assets/img/logo-test.jpg", "logo free");
 elementMenu ("Accueil", "test.html", "home", "1.5", "./assets/img/icone.png", "yes");
 elementMenu ("Accueil2", "accueil.html", "home", "1.5", "./assets/img/icone.png", "" );
-elementMenu ("Accueil3", "baam.html", "home", "1.5" , "", "");
+elementGroupMenu ("Accueil3", "title3", "1.5", "");
+elementSousMenu ("sous menu 3", "lalala.html", "home", "1.5", "", "" );
+elementSousMenu ("sous menu 3", "lalala.html", "home", "1.5", "", "" );
 elementMenu ("Accueil4", "lalala.html", "home", "1.5", "", "" );
 elementMenu ("Accueil5", "lalala.html", "home", "1.5", "", "" );
 elementMenu ("Accueil6", "lalala.html", "home", "1.5", "", "" );
-elementMenu ("Accueil7", "lalala.html", "home", "1.5", "", "" );
-
-load("https://www.belgiantrain.be/fr", "my-element3");
-
-id("my-element").addEventListener("click", ()=> {
-    animateCss('#my-element', 'bounce');
-})
+elementMenu("Accueil7", "title8", "1.5", "");
 
